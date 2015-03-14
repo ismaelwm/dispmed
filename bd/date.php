c04e8112b04292232b27303dd111901ed06a6515
gabbanaesteban <gabbanaesteban@gmail.com>
2015-03-13 20:58:43 -0400

    lkj

--- bd/date.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$d ="15 March, 2015";
-$date= new DateTime($d);
-echo $date->format('Y-m-d');
-
- ?>
--- js/medico.js
+++ js/medico.js
@@ -23,47 +23,45 @@ $(function() {
 		return false;
 	});
 
-	var wasclicked = false, tmp = '';
+	
 	$('#searchButton').click(function() {
 		
 
 		var nombre = $('#buscar').val(), crite = $('#criterio').val(),
 		etiqueta = '<table class= "striped" id=tablaMedico>'+
 		'<tr><th>Nombre</th><th>Cedula</th><th>Tanda</th><th>Especialidad</th><th>Estado</th></tr><tr>';
-		if (tmp != nombre)
-			wasclicked = false;              
 
-		if (wasclicked == false) {
 
-			$.ajax({
-
-				type : 'POST',
-				url : 'bd/buscar_medico.php',
-				data : {buscar:nombre, criterio:crite},
-				success : function(d) {
 		
-					if (d==''){
 
-						toast('No se encontraron coincidencias', 3000);
+		$.ajax({
 
-					}else{
-						var r = d.split('*');
-						for (var i = 0; i < r.length-1; i++) {
+			type : 'POST',
+			url : 'bd/buscar_medico.php',
+			data : {buscar:nombre, criterio:crite},
+			success : function(d) {
 
-							if (r[i] == 'tr')
-								etiqueta += '</tr><tr>';
-							else
-								etiqueta += '<td>'+ r[i]+'</td>';
-						}
-						
-						etiqueta += '</tr></table>';  
-						$('#tablaMedico').html(etiqueta); 
-						wasclicked = true;  
-						tmp = nombre; 
-					} 
-				}
+				if (d==''){
 
-			}); 
-		}
+					toast('No se encontraron coincidencias', 3000);
+
+				}else{
+					var r = d.split('*');
+					for (var i = 0; i < r.length-1; i++) {
+
+						if (r[i] == 'tr')
+							etiqueta += '</tr><tr>';
+						else
+							etiqueta += '<td>'+ r[i]+'</td>';
+					}
+
+					etiqueta += '</tr></table>';  
+					$('#tablaMedico').html(etiqueta); 
+					
+				} 
+			}
+
+		}); 
+		
 	});
 });
